acb8ba961b597af4181831dc29b4fd8dc7f6514e
macbook air <macbookair@MacBook-Air-de-macbook.local>
2016-04-19 00:23:31 +0200

    Yeah men

--- ft_select.h
+++ ft_select.h
@@ -6,22 +6,34 @@
 /*   By: nahmed-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/18 13:44:42 by nahmed-m          #+#    #+#             */
-/*   Updated: 2016/04/18 15:00:06 by nahmed-m         ###   ########.fr       */
+/*   Updated: 2016/04/19 00:17:15 by nahmed-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_SELECT_H
 # define FT_SELECT_H
 
-#include "libft/includes/libft.h"
-#include <termios.h>
-#include <term.h>
-#include <curses.h>
-#include <stdlib.h>
+# include "libft/includes/libft.h"
+# include <termios.h>
+# include <term.h>
+# include <curses.h>
+# include <stdlib.h>
 
-	/*
-	** Utils
-	*/
-	void error(char *str);
+
+# define UP 4283163
+# define DOWN 4348699
+# define RIGHT 4414235
+# define LEFT 4479771
+# define SPACE 32
+# define RETURN 10
+# define ESC 27
+# define BACKSPACE 127
+# define DELETE 2117294875
+/*
+** Utils
+*/
+void error(char *str);
+
+
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nahmed-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/18 13:08:28 by nahmed-m          #+#    #+#             */
-/*   Updated: 2016/04/18 18:11:18 by nahmed-m         ###   ########.fr       */
+/*   Updated: 2016/04/19 00:23:12 by nahmed-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_select.h"
@@ -28,17 +28,26 @@ void		set_cath_mode()
 	tcsetattr(0, TCSAFLUSH, &term_infos);
 }
 
+int		my_outc(int c)
+{
+	ft_putchar(c);
+	return (1);
+}
 
 int main(int argc, char **argv)
 {
-	char				buffer[3];
+	char				buffer[4];
+	unsigned int		keycode;
+	
+	keycode = 0;
 	argc = (int)argv;
 	signal(SIGCONT, set_cath_mode);
 	set_cath_mode();
 	while (1)
 	{
-		read(1, &buffer, 3);
-		ft_printf("%d", buffer);
+		bzero(&buffer, 4);
+		read(1, &buffer, 4);
+		keycode = (buffer[3] << 24) + (buffer[2] << 16) + (buffer[1] << 8) + buffer[0];
 	}
 }
 
